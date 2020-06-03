2e4d4f79f1371ce320202ffbae1a9f6438984dfc
Leonardo <leonardorive6@gmail.com>
2020-06-02 22:53:10 -0500

    Actualización para heroku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/proyectologin'));
+app.use(express.static(__dirname+'/dist/proyectoLogin'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/proyectologin/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/proyectoLogin/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
